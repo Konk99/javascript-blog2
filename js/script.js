df242a1e4bf44ce03881f19d61528cf7c40e2dd8
Kkonk <konk99@o2.pl>
2022-08-12 15:29:04 +0200

    Finish titleClickHandler function

--- js/script.js
+++ js/script.js
@@ -1,30 +1,33 @@
 'use strict';
 
 function titleClickHandler(event) {
+    event.preventDefault();
+    const clickedElement = this;
     /* remove class "active" from all articles links */
     const activeLinks = document.querySelectorAll(".titles a.active");
 
     for(let activeLink of activeLinks) {
         activeLink.classList.remove("active");
     }
-    
+
     /* add class "active" to the clicked link */
+    clickedElement.classList.add("active");
+    
+    /* remove class "active" from all articles */
     const activeArticles = document.querySelectorAll(".posts article.active");
 
     for(let activeArticle of activeArticles) {
         activeArticle.classList.remove("active");
     }
     
-    /* remove class "active" from all articles */
-
-    
-    /* get "hred" attribute from clicked link */
-
+    /* get "href" attribute from clicked link */
+    let linkHref = clickedElement.getAttribute("href");
     
     /* find the correct article using the selector (value of "href attribute") */
-
+    const correctArticle = document.querySelector(linkHref);
     
     /* add class "active" to the correct article */
+    correctArticle.classList.add("active");
 }
 
 const links = document.querySelectorAll(".titles a");
